1ce5fa88ff24774178c30396ccf02c9c5f11ab75
Mateusz Chiliński <mateusz@chilinski.eu>
2017-04-12 07:35:04 +0200

    new tasks

---
+++ Lab8/Lab8/GraphColoring.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ASD.Graphs;
+
+namespace ASD
+{
+    public static class GraphColoring
+    {
+        /// <summary>
+        /// Ogólna metoda - kolorowanie zachłanne na podstawie ustalonego ciągu wierzchołków.
+        /// </summary>
+        /// <param name="g">Graf do pokolorowania</param>
+        /// <param name="order">Porządek wierzchołków, w jakim mają być one przetwarzane. W przypadku null użyj pierwotnego numerowania wierzchołków</param>
+        /// <returns>Tablica kolorów: na i-tej pozycji ma być kolor i-tego wierzchołka. Kolory numerujemy od 1.</returns>
+        /// <remarks>
+        /// 1) Algorytm powinien działać dla grafów nieskierowanych, niespójnych
+        /// 2) Grafu nie wolno zmieniać
+        /// </remarks>
+        public static int[] GreedyColoring(this Graph g, int[] order = null)
+        {
+            Graph gC = g.Clone();
+            int[] colours = new int[gC.VerticesCount];
+            if (order == null)
+            {
+                order = new int[gC.VerticesCount];
+                for (int i = 0; i < gC.VerticesCount; i++)
+                {
+                    order[i] = i;
+                }
+            }
+            foreach (var i in order)
+            {
+                HashSet<int> usedColours = new HashSet<int>();
+                int largestColour = 1;
+                foreach (var edge in gC.OutEdges(i))
+                {
+                    if (largestColour <= colours[edge.To])
+                        largestColour = colours[edge.To] + 1;
+                    usedColours.Add(colours[edge.To]);
+                }
+                for (int j = 1; j <= largestColour; j++)
+                {
+                    if (!usedColours.Contains(j))
+                    {
+                        colours[i] = j;
+                        break;
+                    }
+                }
+            }
+            return colours;
+        }
+
+        /// <summary>
+        /// Przybliżone kolorowanie metodą BFS
+        /// </summary>
+        /// <param name="g">Graf do pokolorowania</param>
+        /// <returns>Tablica kolorów: na i-tej pozycji ma być kolor i-tego wierzchołka. Kolory numerujemy od 1.</returns>
+        /// <remarks>
+        /// 1) Algorytm powinien działać dla grafów nieskierowanych, niespójnych
+        /// 2) Grafu nie wolno zmieniać
+        /// </remarks>
+        static int[] sColors;
+        static Graph gC;
+        public static int[] BFSColoring(this Graph g)
+        {
+            gC = g.Clone();
+            int cc;
+            sColors = new int[gC.VerticesCount];
+            gC.GeneralSearchAll<EdgesQueue>(visitEdgeF, null, null, out cc);
+            return sColors;
+        }
+
+        private static bool visitEdgeF(int i)
+        {
+            HashSet<int> usedColours = new HashSet<int>();
+            int largestColour = 1;
+            foreach (var edge in gC.OutEdges(i))
+            {
+                if (largestColour <= sColors[edge.To])
+                    largestColour = sColors[edge.To] + 1;
+                usedColours.Add(sColors[edge.To]);
+            }
+            for (int j = 1; j <= largestColour; j++)
+            {
+                if (!usedColours.Contains(j))
+                {
+                    sColors[i] = j;
+                    break;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Przybliżone kolorowanie metodą LargestBackDegree - niedziałająca
+        /// </summary>
+        /// <param name="g">Graf do pokolorowania</param>
+        /// <returns>Tablica kolorów: na i-tej pozycji ma być kolor i-tego wierzchołka. Kolory numerujemy od 1.</returns>
+        /// <remarks>
+        /// 1) Algorytm powinien działać dla grafów nieskierowanych, niespójnych
+        /// 2) Grafu nie wolno zmieniać
+        /// </remarks>
+        public static int[] LargestBackDegree(this Graph g)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Przybliżone kolorowanie metodą ColorDegreeOrdering
+        /// </summary>
+        /// <param name="g">Graf do pokolorowania</param>
+        /// <returns>Tablica kolorów: na i-tej pozycji ma być kolor i-tego wierzchołka. Kolory numerujemy od 1.</returns>
+        /// <remarks>
+        /// 1) Algorytm powinien działać dla grafów nieskierowanych, niespójnych
+        /// 2) Grafu nie wolno zmieniać
+        /// </remarks>
+        public static int[] ColorDegreeOrdering(this Graph g)
+        {
+            int[] colourDegree = new int[g.VerticesCount];
+            int[] colours = new int[g.VerticesCount];
+            while (colourDegree.Max() != -1)
+            {
+                int highest = colourDegree.Max();
+                int curr = -1;
+                for (int i = 0; i < colourDegree.Length; i++)
+                {
+                    if (colourDegree[i] == highest)
+                    {
+                        curr = i;
+                        colourDegree[i] = -1;
+                        foreach (var edge in g.OutEdges(i))
+                        {
+                            if (colourDegree[edge.To] != -1)
+                                colourDegree[edge.To]++;
+                        }
+
+                        HashSet<int> usedColours = new HashSet<int>();
+                        int largestColour = 1;
+                        foreach (var edge in g.OutEdges(i))
+                        {
+                            if (largestColour <= colours[edge.To])
+                                largestColour = colours[edge.To] + 1;
+                            usedColours.Add(colours[edge.To]);
+                        }
+                        for (int j = 1; j <= largestColour; j++)
+                        {
+                            if (!usedColours.Contains(j))
+                            {
+                                colours[i] = j;
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+            return colours;
+        }
+
+        /// <summary>
+        /// Przybliżone kolorowanie metodą Incremental
+        /// </summary>
+        /// <param name="g">Graf do pokolorowania</param>
+        /// <returns>Tablica kolorów: na i-tej pozycji ma być kolor i-tego wierzchołka. Kolory numerujemy od 1.</returns>
+        /// <remarks>
+        /// 1) Algorytm powinien działać dla grafów nieskierowanych, niespójnych
+        /// 2) Grafu nie wolno zmieniać
+        /// </remarks>
+        public static int[] Incremental(this Graph g)
+        {
+            int[] colours = new int[g.VerticesCount];
+
+            int c = 1;
+            while (colours.Min() == 0)
+            {
+                HashSet<int> usedVerts = new HashSet<int>();
+                for (int i = 0; i < g.VerticesCount; i++)
+                {
+                    if (colours[i] != 0)
+                        continue;
+                    bool add = true;
+                    foreach (var edge in g.OutEdges(i))
+                    {
+                        if (usedVerts.Contains(edge.To))
+                        {
+                            add = false;
+                            break;
+                        }
+                    }
+                    if (add)
+                    {
+                        usedVerts.Add(i);
+                        colours[i] = c;
+                    }
+                }
+                c++;
+            }
+            return colours;
+        }
+    }
+}
---
+++ asd2-lab7/asd2-lab7/BridgeCrossing.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ASD
+{
+    public class BridgeCrossing
+    {
+        /// <summary>
+        ///     Metoda rozwiązuje zadanie optymalnego przechodzenia przez most.
+        /// </summary>
+        /// <param name="_times">Tablica z czasami przejścia poszczególnych osób</param>
+        /// <param name="strategy">
+        ///     Strategia przekraczania mostu: lista list identyfikatorów kolejnych osób,
+        ///     które przekraczają most (na miejscach parzystych przejścia par przez most,
+        ///     na miejscach nieparzystych powroty jednej osoby z latarką). Jeśli istnieje więcej niż jedna strategia
+        ///     realizująca przejście w optymalnym czasie wystarczy zwrócić dowolną z nich.
+        /// </param>
+        /// <returns>Minimalny czas, w jakim wszyscy turyści mogą pokonać most</returns>
+        public static int minTime;
+
+        public static List<List<int>> minList;
+        public static Dictionary<int, int> timesTo;
+
+        public static int CrossBridge(int[] times, out List<List<int>> strategy)
+        {
+            if (times.Length == 1)
+            {
+                strategy = new List<List<int>>();
+                var z = new List<int>();
+                z.Add(0);
+                strategy.Add(z);
+                return times[0];
+            }
+            minTime = int.MaxValue;
+            minList = new List<List<int>>();
+            timesTo = new Dictionary<int, int>();
+            var k = 0;
+            foreach (var time in times)
+                if (!timesTo.ContainsKey(time))
+                    timesTo.Add(time, k++);
+            var beforeB = new Dictionary<int, int>();
+            var afterB = new Dictionary<int, int>();
+            for (var i = 0; i < times.Length; i++)
+                if (beforeB.ContainsKey(times[i]))
+                    beforeB[times[i]] += 1;
+                else
+                    beforeB.Add(times[i], 1);
+            Calculate(beforeB, afterB, 0, false, minList);
+            var usedV = new bool[times.Length];
+            foreach (var smallList in minList)
+            {
+                var changeTo = -1;
+                if (smallList.Count == 1)
+                {
+                    for (var i = 0; i < times.Length; i++)
+                    {
+                        if (times[i] == beforeB.ElementAt(smallList[0]).Key && usedV[i])
+                        {
+                            smallList[0] = i;
+                            usedV[i] = false;
+                            break;
+                        }
+                    }
+                    continue;
+                }
+                if (smallList[0] == smallList[1])
+                {
+                    var n = 0;
+                    var search_for = smallList[0];
+                    for (var i = 0; i < times.Length; i++)
+                    {
+                        if (times[i] == beforeB.ElementAt(search_for).Key && !usedV[i])
+                        {
+                            smallList[n++] = i;
+                            usedV[i] = true;
+                        }
+                        if (n > 1)
+                            break;
+                    }
+                    continue;
+                }
+                int z = 0;
+                int[] c = new int[2];
+                c[0] = smallList[0];
+                c[1] = smallList[1];
+                foreach (var smallElement in smallList)
+                {
+                    for (var i = 0; i < times.Length; i++)
+                        if (times[i] == beforeB.ElementAt(smallElement).Key && !usedV[i])
+                        {
+                            usedV[i] = true;
+                            c[z++] = i;
+                            break;
+                        }
+                }
+                smallList[0] = c[0];
+                smallList[1] = c[1];
+            }
+            strategy = minList;
+            return minTime;
+        }
+
+        public static int findMinKey(Dictionary<int, int> afterB)
+        {
+            var min = int.MaxValue;
+            foreach (var k1 in afterB)
+                if (min > k1.Key)
+                    min = k1.Key;
+            return min;
+        }
+
+        public static void goToOtherSide(Dictionary<int, int> beforeB, Dictionary<int, int> afterB, int k1, bool rtrn)
+        {
+            if (rtrn)
+            {
+                if (beforeB.ContainsKey(k1))
+                    beforeB[k1] += 1;
+                else
+                    beforeB.Add(k1, 1);
+                if (afterB.ContainsKey(k1) && afterB[k1] == 1)
+                    afterB.Remove(k1);
+                else
+                    afterB[k1]--;
+            }
+            else
+            {
+                if (afterB.ContainsKey(k1))
+                    afterB[k1] += 1;
+                else
+                    afterB.Add(k1, 1);
+                if (beforeB.ContainsKey(k1) && beforeB[k1] == 1)
+                    beforeB.Remove(k1);
+                else
+                    beforeB[k1]--;
+            }
+        }
+
+        public static void Calculate(Dictionary<int, int> beforeB, Dictionary<int, int> afterB, int currTime, bool rtrn,
+            List<List<int>> currList)
+        {
+            if (currTime > minTime) return;
+            if (beforeB.Count == 0)
+            {
+                if (currTime < minTime)
+                {
+                    minList = currList;
+                    minTime = currTime;
+                }
+                return;
+            }
+
+            if (!rtrn)
+            {
+                for (var i = 0; i < beforeB.Count; i++)
+                for (var j = i; j < beforeB.Count; j++)
+                {
+                    var k1 = beforeB.ElementAt(i);
+                    var k2 = beforeB.ElementAt(j);
+                    if (k1.Equals(k2) && k1.Value < 2)
+                        continue;
+                    var beforeB_temp = new Dictionary<int, int>(beforeB);
+                    var afterB_temp = new Dictionary<int, int>(afterB);
+                    var tempList = new List<List<int>>(currList);
+                    var tmpL = new List<int>();
+                    tmpL.Add(timesTo[k1.Key]);
+                    tmpL.Add(timesTo[k2.Key]);
+                    tempList.Add(tmpL);
+                    goToOtherSide(beforeB_temp, afterB_temp, k1.Key, false);
+                    goToOtherSide(beforeB_temp, afterB_temp, k2.Key, false);
+                    if (currTime + Math.Max(k1.Key, k2.Key) > minTime) continue;
+                    currTime += Math.Max(k1.Key, k2.Key);
+                    Calculate(beforeB_temp, afterB_temp, currTime, true, tempList);
+                    currTime -= Math.Max(k1.Key, k2.Key);
+                }
+            }
+            else
+            {
+                var beforeB_temp = new Dictionary<int, int>(beforeB);
+                var afterB_temp = new Dictionary<int, int>(afterB);
+                var currDude = findMinKey(afterB_temp);
+                var tempList = new List<List<int>>(currList);
+                var tmpL = new List<int>();
+                tmpL.Add(timesTo[currDude]);
+                tempList.Add(tmpL);
+                goToOtherSide(beforeB_temp, afterB_temp, currDude, true);
+                currTime += currDude;
+                Calculate(beforeB_temp, afterB_temp, currTime, false, tempList);
+                currTime -= currDude;
+            }
+        }
+
+        // MOŻESZ NAWET DODAĆ CAŁE KLASY (ALE NIE MUSISZ)
+        // MOŻESZ DOPISAĆ POTRZEBNE POLA I METODY POMOCNICZE
+    }
+}
