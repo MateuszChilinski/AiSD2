209579e27018017a5cfb69b336ae8144b08e0aec
Mateusz Chiliński <mateusz@chilinski.eu>
2017-05-11 22:29:24 +0200

    almost completed

--- asd2-lab11/asd2-lab11/AlmostMatching.cs
+++ asd2-lab11/asd2-lab11/AlmostMatching.cs
@@ -1,6 +1,7 @@
 
 using System.Collections.Generic;
 using ASD.Graphs;
+using System.Linq;
 
 namespace lab10
 {
@@ -31,8 +32,9 @@ public class AlmostMatching
         /// <param name="g">Graf linek</param>
         /// <param name="allowedCollisions">Limit ryzyka</param>
         static Graph realG;
-        static int maximum = 0;
+        static int max = 0;
         static int allC = 0;
+        static double min;
         static List<Edge> bestList;
         public static AlmostMatchingSolution LargestS(Graph g, int allowedCollisions)
             {
@@ -43,41 +45,64 @@ public class AlmostMatching
             {
                 foreach (var e in realG.OutEdges(i))
                 {
-                    if(!allEdges.Contains(e) && !allEdges.Contains(new Edge(e.To, e.From)))
+                    if(!allEdges.Contains(e) && !allEdges.Contains(new Edge(e.To, e.From, e.Weight)))
                         allEdges.Add(e);
                 }
             }
+            max = 0;
+            min = double.MaxValue;
+            bestList = null;
             List<Edge> edgeList = new List<Edge>();
             List<Edge> myEdgeList = new List<Edge>();
-            int z = Search(edgeList, allEdges);
-             return new AlmostMatchingSolution(z, bestList);
+            int[] verts = new int[g.VerticesCount];
+            int used=0;
+            int z = Search(edgeList, allEdges, verts, used);
+            return new AlmostMatchingSolution(max, bestList);
             }
-        public static int Search(List<Edge> prevList, List<Edge> allEdges)
+        public static int Search(List<Edge> prevList, List<Edge> allEdges, int[] verts, int used)
         {
-            if(prevList.Count >= realG.VerticesCount/2 + allC || allEdges.Count == 0)
+            if(allEdges.Count == 0 && used <= allC)
             {
-                bestList = new List<Edge>(prevList);
+                if (max <= prevList.Count)
+                {
+                    if (max == prevList.Count)
+                    {
+                        double tmp_min = (from s in prevList
+                                         select s.Weight).Sum();
+                        if (tmp_min < min)
+                        {
+                            min = tmp_min;
+                            bestList = new List<Edge>(prevList);
+                        }
+                    }
+                    else
+                    {
+                        max = prevList.Count;
+                        bestList = new List<Edge>(prevList);
+                    }
+                }
                 return prevList.Count;
             }
-            int max = 0;
-                Edge e = allEdges[0];
-                allEdges.Remove(e);
-                prevList.Add(e);
-                int tmp = Search(prevList, allEdges);
-                if (tmp > max)
-                {
-                    if (tmp > maximum)
-                    {
-                        maximum = tmp;
-                    }
-                    max = tmp;
-                }
-                else
-                {
-                    allEdges.Add(e);
-                }
-                prevList.Remove(e);
-            return max;
+            else if(used > allC)
+            {
+                return -1;
+            }
+            Edge e = allEdges[0];
+            allEdges.Remove(e);
+            prevList.Add(e);
+            bool from = false;
+            bool to = false;
+            if (++verts[e.From] > 1) { used++; from = true; }
+            if (++verts[e.To] > 1) { used++; to = true; }
+            Search(new List<Edge>(prevList), new List<Edge>(allEdges), verts, used);
+            prevList.Remove(e);
+            --verts[e.From];
+            --verts[e.To];
+            if (from) { used--; }
+            if (to) { used--; }
+            Search(new List<Edge>(prevList), new List<Edge>(allEdges), verts, used);
+            
+            return prevList.Count;
         }
     }
 
